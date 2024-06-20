8e0530b1da2bbaa9b8718e74c1a008f7f4e722ea
SandeepDinodia <151106767+SandeepDinodia@users.noreply.github.com>
2024-06-20 14:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age)
+	this.name=name,
+	this.age=age
+}
+get name(){
+	return this.name
+}
+set age(){
+	return this.age
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study(){
+		return `${this.name} is studying`
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach(){
+		return `${this.name} is teaching`
+	}
+}
+const student= new student("sandeep",28);
+student.study();
+
+const teacher = new teacher("rishabh",30);
+teacher.teach();
 
 // Do not change the code below this line
 window.Person = Person;
